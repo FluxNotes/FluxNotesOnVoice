49d5632167d2a4430ccba2c9039a20c34e5e2508
David Tresner-Kirsch <dwkirsch@gmail.com>
2018-07-18 22:11:34 -0400

    abstract out the data payload and api call

--- watson4fluxnotes.rb
+++ watson4fluxnotes.rb
@@ -5,14 +5,19 @@ require 'json'
 class Watson4Fluxnotes
   
   def example_call
-    data = {
-      "text" => "I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up and live out the true meaning of its creed: \"We hold these tru    ths to be self-evident, that all men are created equal.\"",
-      "features" => {
-        "sentiment" => {},
-        "keywords" => {}
-      }
-    }.to_json
+    feature_list = ["sentiment", "keywords"]
+    data = construct_data "I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up and live out the true meaning of its creed: \"We hold these truths to be self-evident, that all men are created equal.\"", feature_list
     
+  end
+
+  def analyze_text (text)
+    feature_list = ["entities", "concepts"]
+    data = construct_data text, feature_list
+    api_call_analyze data
+  end
+
+  def api_call_analyze (data)
+    # data is expected to be a json hash containing both the text to be analyzed and the desired features, as constructed by #construct_data()
     url = URI.parse("https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-03-19")
     req = Net::HTTP::Post.new(url.to_s, initheader = {'Content-Type' =>'application/json'})
     req.body = data
@@ -22,10 +27,20 @@ class Watson4Fluxnotes
     }
     puts res.body
   end
-
+  
+  def construct_data (text, feature_list)
+    feature_hash = {}
+    feature_list.each{|f| feature_hash[f] = {}}
+    data = {
+      "text" => text,
+      "features" => feature_hash
+    }
+    return data.to_json
+  end
+  
 end
 
 watson = Watson4Fluxnotes.new
 
-watson.example_call
+watson.analyze_text "and well the I'm sorry to hear that you're having this that you're experiencing this is myalgias in this this this neuropathy chances are on this is a side effect for a toxicity associated with the the the taxol and a tweet we do see this every now and then"
 
