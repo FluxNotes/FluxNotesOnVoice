0e0a6408e15534bacb918ee8b4c3fd715f5d43f6
David Tresner-Kirsch <dwkirsch@gmail.com>
2018-07-18 21:56:26 -0400

    create data json from ruby data structures, rather than defining json in a literal string

--- watson4fluxnotes.rb
+++ watson4fluxnotes.rb
@@ -1,20 +1,17 @@
 require 'net/http'
+require 'json'
 
-
-watson = Watson4Fluxnotes.new
-
-watson.example_call
 
 class Watson4Fluxnotes
-
+  
   def example_call
-    data = '{
-      "text": "I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up and live out the true meaning of its creed: \"We hold these tru    ths to be self-evident, that all men are created equal.\"",
-      "features": {
-        "sentiment": {},
-        "keywords": {}
+    data = {
+      "text" => "I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up and live out the true meaning of its creed: \"We hold these tru    ths to be self-evident, that all men are created equal.\"",
+      "features" => {
+        "sentiment" => {},
+        "keywords" => {}
       }
-    }'
+    }.to_json
     
     url = URI.parse("https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-03-19")
     req = Net::HTTP::Post.new(url.to_s, initheader = {'Content-Type' =>'application/json'})
@@ -27,3 +24,8 @@ class Watson4Fluxnotes
   end
 
 end
+
+watson = Watson4Fluxnotes.new
+
+watson.example_call
+
