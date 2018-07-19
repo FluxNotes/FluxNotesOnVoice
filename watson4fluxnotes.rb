8b4b3c657d2ac42024485f0b9902a5f338423b73
David Tresner-Kirsch <dwkirsch@gmail.com>
2018-07-18 22:14:53 -0400

    move execution out of class file, into script

--- watson4fluxnotes.rb
+++ watson4fluxnotes.rb
@@ -40,7 +40,4 @@ class Watson4Fluxnotes
   
 end
 
-watson = Watson4Fluxnotes.new
-
-watson.analyze_text "and well the I'm sorry to hear that you're having this that you're experiencing this is myalgias in this this this neuropathy chances are on this is a side effect for a toxicity associated with the the the taxol and a tweet we do see this every now and then"
 
