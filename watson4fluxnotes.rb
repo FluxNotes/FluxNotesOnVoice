fe0bcb52dfa65c099c4ffc3114a1324c204c348a
David Tresner-Kirsch <dwkirsch@gmail.com>
2018-07-18 21:43:40 -0400

    upgrade example.com call to example watson call

--- watson4fluxnotes.rb
+++ watson4fluxnotes.rb
@@ -1,8 +1,18 @@
 require 'net/http'
 
-url = URI.parse('http://www.example.com/index.html')
-req = Net::HTTP::Get.new(url.to_s)
-res = Net::HTTP.start(url.host, url.port) {|http|
+data = '{
+  "text": "I still have a dream. It is a dream deeply rooted in the American dream. I have a dream that one day this nation will rise up and live out the true meaning of its creed: \"We hold these truths to be self-evident, that all men are created equal.\"",
+  "features": {
+    "sentiment": {},
+    "keywords": {}
+  }
+}'
+
+url = URI.parse("https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-03-19")
+req = Net::HTTP::Post.new(url.to_s, initheader = {'Content-Type' =>'application/json'})
+req.body = data
+req.basic_auth( "43ed518a-ca46-44f1-ba5f-442e636ce982", "Dhc0QsjieyTB")
+res = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') {|http|
   http.request(req)
 }
 puts res.body
