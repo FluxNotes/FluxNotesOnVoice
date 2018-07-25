d731d505c1628c4cdb35e270ef1ab5d64bceaf9b
David Tresner-Kirsch <dwkirsch@gmail.com>
2018-07-25 18:04:50 -0400

    add support for specifying a list of components to use

--- info-extraction/analyze_toxicity_in_full_text.rb
+++ info-extraction/analyze_toxicity_in_full_text.rb
@@ -11,7 +11,8 @@ opts = Trollop::options do
   opt :text, "single string to analyze", :type => :string        # string --text <s>, default nil
   opt :file, "file containing line-by-line strings to be analyzed", :type => :io
   opt :output, "destination for analyzed data (default STDOUT)", :type => :string
-  opt :outputFormat, "output format; values: json, spreadsheet", :type => :string, :default => "json"
+  opt :outputFormat, "output format; values: spreadsheet", :type => :string, :default => "spreadsheet"
+  opt :components, "which analytic components to apply; possible values: watson, meddra", :type => :strings, :default => ["watson"]
   opt :meddra_score_threshold, "only keep disease mentions that score higher than this", :type => :float, :default => 6.5
 end
 
@@ -34,8 +35,8 @@ end
 
 
 chunker = Chunker.new
-watson = Watson4Fluxnotes.new
-meddra = Meddra4Fluxnotes.new
+watson = Watson4Fluxnotes.new if opts[:components].include? "watson"
+meddra = Meddra4Fluxnotes.new if opts[:components].include? "meddra"
 collector = FindingsCollector.new
 
 
@@ -43,43 +44,45 @@ texts.each do |text|
   puts "ANALYZING TEXT"
   results = []
   chunker.run text, :toxicity # find the chunks of text related to toxicity
-  
-  # analyze the chunks via Watson, and filter the results down to relevant findings:
 
-  chunker.chunks.map do |chunk|
-    puts "sending to Watson: #{chunk}"
-    watson.analyze_text(chunk)
-    
-  end.each do |result| # each result is the Watson output for the given chunk (after various filtering)
-    # collect what we need from the extracted features
-    result["entities"].each do |e|
-      collector << {:text => e["text"],
-                    :analytic_source => "Watson",
-                    :feature => "Entity",
-                    :type => e["type"],
-                    :score => e["relevance"],
-                    :context => result["analyzed_text"]}
-    end
-    result["concepts"].each do |c|
-      collector << {:text => c["text"],
-                    :analytic_source => "Watson",
-                    :feature => "Concept",
-                    :code => c["dbpedia_resource"].sub("http://dbpedia.org/resource/", ""),
-                    :score => c["relevance"],
-                    :context => result["analyzed_text"]}
+  if opts[:components].include? "watson"
+    # analyze the chunks via Watson, and filter the results down to relevant findings:
+    chunker.chunks.map do |chunk|
+      puts "sending to Watson: #{chunk}"
+      watson.analyze_text(chunk)
+      
+    end.each do |result| # each result is the Watson output for the given chunk (after various filtering)
+      # collect what we need from the extracted features
+      result["entities"].each do |e|
+        collector << {:text => e["text"],
+                      :analytic_source => "Watson",
+                      :feature => "Entity",
+                      :type => e["type"],
+                      :score => e["relevance"],
+                      :context => result["analyzed_text"]}
+      end
+      result["concepts"].each do |c|
+        collector << {:text => c["text"],
+                      :analytic_source => "Watson",
+                      :feature => "Concept",
+                      :code => c["dbpedia_resource"].sub("http://dbpedia.org/resource/", ""),
+                      :score => c["relevance"],
+                      :context => result["analyzed_text"]}
+      end
     end
   end
   
-  
-  # analyze the chunks via MedDRA, and filter results down to high-scoring findings:
-  chunker.chunks.each do |chunk|
-    puts "sending to MedDRA: #{chunk}"
-    concepts = meddra.analyze_text(chunk, opts[:meddra_score_threshold])
-    
-    concepts.each do |c|
-      collector << {:text => c["term"], :analytic_source => "MedDRA", :code => c["code"], :score => c["score"], :context => chunk}
+  if opts[:components].include? "meddra"
+    # analyze the chunks via MedDRA, and filter results down to high-scoring findings:
+    chunker.chunks.each do |chunk|
+      puts "sending to MedDRA: #{chunk}"
+      concepts = meddra.analyze_text(chunk, opts[:meddra_score_threshold])
+      
+      concepts.each do |c|
+        collector << {:text => c["term"], :analytic_source => "MedDRA", :code => c["code"], :score => c["score"], :context => chunk}
+      end
+      
     end
-    
   end
   
   # write what we've found
