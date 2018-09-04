6ed7437e25f9c58c6be6ae7799f9e957c40a403f
David Tresner-Kirsch <dwkirsch@gmail.com>
2018-09-04 17:00:52 -0400

    detect more rationale expressions; normalize both rationale assertions and status assertions; new data schema for output

--- info-extraction/lib/disease_status_extractor.rb
+++ info-extraction/lib/disease_status_extractor.rb
@@ -11,31 +11,97 @@ It will also do the right thing in slightly more complicated cases, for example,
 
 =end
 
+=begin
+Example output:
+[
+    {
+	"disease": null,
+	"status": {
+	    "mention text": "not changing",
+	    "normalized": "stable"
+	},
+	"rationale": [
+	    {
+		"mention text": "CT scans",
+		"normalized": "Imaging"
+	    }
+	]
+    },
+    {
+	"disease": null,
+	"status": {
+	    "mention text": "progressing",
+	    "normalized": "progressing"
+	},
+	"rationale": [
+	    {
+		"mention text": "CT scans",
+		"normalized": "Imaging"
+	    },
+	    {
+		"mention text": "physical exam",
+		"normalized": "Physical Exam"
+	    }
+	]
+    }
+]
+=end
+
 class DiseaseStatusExtractor
   def analyze_text (text)
     annotated = Standoff::AnnotatedString.new( :signal => text, :tags => [])
 
     # this would be just key.match document, but we want MatchData for possible multiple keyword matches, not just one
-    annotated.signal.to_enum(:scan, /status|disease|cancer/).map{ Regexp.last_match }.each do |match|
+    annotated.signal.to_enum(:scan, /status|disease|cancer/i).map{ Regexp.last_match }.each do |match|
       annotated.tags << Standoff::Tag.new(:content => match[0],
                                           :name => "disease_status_key",
                                           :start => match.begin(0),
                                           :end => match.end(0) )
     end
 
-    annotated.signal.to_enum(:scan, /(not )?(stable|progressing|getting worse|worsening|getting better|improving)/).map{ Regexp.last_match }.each do |match|
+    # TODO: this is dopey. duplication of search expressions between here and the disease_status patterns in Chunker should be abstracted and merged.
+    annotated.signal.to_enum(:scan, /((not? )|(complete ))?(stable|progressing|responding|response( to treatment)?|resection|inevaluable|changed?|getting worse|worsening|getting better|improving)/i).map{ Regexp.last_match }.each do |match|
+      mention_text = match[0]
+      mapped_for_normalization = case mention_text # be very careful with this. it evaluates greedily, and as such the order of expressions here matters a lot.
+                                 when /^not? /i
+                                   "stable"
+                                 when /getting worse|worsening|progress/i
+                                   "progressing"
+                                 when /complete resection/i, /complete response/i
+                                   $&
+                                 when /getting better|improving|response to treatment/i
+                                   "responding"
+                                 when /inevaluable/i, /stable/i, /progressing/i
+                                   $&
+                                 else
+                                   nil
+                                 end
+      normalized = mapped_for_normalization.split(/ |\_/).map(&:capitalize).join(" ") #cap each word
       annotated.tags << Standoff::Tag.new(:content => match[0],
                                           :name => "status_value",
+                                          :attributes => {:normalized => normalized},
                                           :start => match.begin(0),
                                           :end => match.end(0) )
     end
 
-    annotated.signal.to_enum(:scan, /((CT scan|imaging|lab|pathology)( results)?)|symptoms?|exam|markers/).map{ Regexp.last_match }.each do |match|
+    annotated.signal.to_enum(:scan, /((ca?t scan|mri|x-ray|x ray|imaging)( results)?)/i).map{ Regexp.last_match }.each do |match|
       annotated.tags << Standoff::Tag.new(:content => match[0],
                                           :name => "status_rationale",
+                                          :attributes => {:normalized => "Imaging"},
                                           :start => match.begin(0),
                                           :end => match.end(0) )
     end
+    annotated.signal.to_enum(:scan, /(pathology|symptoms|(physical )?exam|markers)/i).map{ Regexp.last_match }.each do |match|
+      mention_text = m[0]
+      mention_text = "physical exam" if mention_text == "exam"
+      normalized = mention_text.split(/ |\_/).map(&:capitalize).join(" ") #cap each word
+      annotated.tags << Standoff::Tag.new(:content => mention_text,
+                                          :name => "status_rationale",
+                                          :attributes => {:normalized => normalized},
+                                          :start => match.begin(0),
+                                          :end => match.end(0) )
+    end
+    
 
     
 
@@ -46,8 +112,12 @@ class DiseaseStatusExtractor
       if next_tag && (next_tag.name == "status_value")
         disease_status_assertions << {
           :disease => nil,
-          :rationale => annotated.tags.select{|tag| tag.name == "status_rationale"}.map{|tag| tag.content}.uniq,
-          :status => next_tag.content
+          :rationale => annotated.tags.select{|tag| tag.name == "status_rationale"}.map do|tag|
+            {:mention_text => tag.content, :normalized => tag.attributes[:normalized]}
+          end.uniq,
+          :status => { :mention_text => next_tag.content,
+                       :normalized => next_tag.attributes[:normalized]}
+            
         }
       end
     end
